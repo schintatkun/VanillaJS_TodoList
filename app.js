75200746949a3b6ae79d6ecf68f382f093c991f6
Shane Chintatkun <schintatkun@gmail.com>
2020-08-04 00:29:05 -0700

    added event listener deleteCheck and deleteCheck function, now todoList can be deleted

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo); 
+todoList.addEventListener('click', deleteCheck);
+
 
 //Functions
 function addTodo(event){
@@ -38,4 +40,13 @@ function addTodo(event){
     
     //Clear TodoInput value
     todoInput.value = "";
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    //Delete TODO
+    if (item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        todo.remove();
+    }
 }
