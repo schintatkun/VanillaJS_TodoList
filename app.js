a5cc55d2bad8cfa2d43b44694673413f21cc2b55
Shane Chintatkun <schintatkun@gmail.com>
2020-08-04 21:42:18 -0700

    finished completed <check mark> button functionality

--- app.js
+++ app.js
@@ -49,4 +49,10 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.remove();
     }
+
+    //Check MARK 
+    if (item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
